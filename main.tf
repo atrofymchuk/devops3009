dd89bd25f6978e88b706fb0b19553a195e2cd39f
Andrii Trofymchuk <expert.it.andrey@gmail.com>
2025-10-05 20:31:09 +0300

    added server node-1

--- main.tf
+++ main.tf
@@ -13,3 +13,9 @@ provider "hcloud" {
   token = var.hcloud_token 
 }
 
+resource "hcloud_server" "server" {
+  name = "node-1"
+  server_type = "cpx11"
+  location = "hel1"
+  image = "ubuntu-24.04"
+}
